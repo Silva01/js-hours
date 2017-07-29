fec971004372c579010d233aca12f8caa9b891b4
Daniel Silva <silva20102@gmail.com>
2017-07-29 20:01:32 -0300

    Adicionado arquivo horasCalc.ts

--- src/constantes.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Created by DanielSilva on 28/07/17.
- */
-
-const FORMATO_HORA = ":";
-const FORMATO_MINUTOS = "M";
-const FORMATO_SEGUNDOS = "s";
-const FORMATO_MILISSEGUNDOS = "m";
---
+++ src/horasCalc.ts
@@ -0,0 +1,3 @@
+/**
+ * Created by DanielSilva on 29/07/17.
+ */
